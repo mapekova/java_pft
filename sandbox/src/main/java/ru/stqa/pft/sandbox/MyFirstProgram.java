2bb835100c11089fc22457f944093a9f525c6d81
mapekova <45543158+mapekova@users.noreply.github.com>
2019-01-29 13:22:39 -0800

    few functions was aded

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,10 +1,31 @@
 package ru.stqa.pft.sandbox;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class MyFirstProgram {
-	public static void main(String args[])
-	{
-    	System.out.println("Hello, world!");
-	}
+
+    public static void main(String args[]) {
+        hello("world");
+
+        double l = 5;
+        System.out.println("Area of square with side" + l + "=" + area(l));
+
+        double a =4;
+        double b =6;
+        System.out.println("Area of rectangle" + a + "and" + b + "=" + area(a,b));
+    }
+    public static void hello(String somebody) {
+        System.out.println("Hello," + somebody + "!");
+
+    }
+    public static double area(double len) {
+        return len*len;
+    }
+
+    public static double area(double a, double b) {
+        return a*b;
+
+    }
 }
 
 
